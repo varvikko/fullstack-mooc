22e1386a8264f80cbc10efe967d598d0916de818
Nuutti Varvikko <nvarvikko@gmail.com>
2020-10-18 14:56:23 +0300

    3.7

--- osa3/puhelinluettelo-backend/index.js
+++ osa3/puhelinluettelo-backend/index.js
@@ -1,9 +1,11 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+var morgan = require('morgan')
 
 var app = express()
 
 app.use(bodyParser.json())
+app.use(morgan('tiny'))
 
 app.listen(3000, () => {
     console.log('Listening')
