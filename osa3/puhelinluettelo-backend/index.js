b9a6d30f0eaf725a58ee6c26cccc6daf6f651d0f
Nuutti Varvikko <nvarvikko@gmail.com>
2020-10-18 19:04:05 +0300

    3.22

--- osa3/puhelinluettelo-backend/index.js
+++ osa3/puhelinluettelo-backend/index.js
@@ -1,123 +1,101 @@
-var express = require('express')
-var bodyParser = require('body-parser')
-var cors = require('cors')
-var morgan = require('morgan')
+var express = require("express");
+var bodyParser = require("body-parser");
+var cors = require("cors");
+var morgan = require("morgan");
 
-var db = require('./db')
+var db = require("./db");
 
-var app = express()
+var app = express();
 
-morgan.token('content-length', function getContentLength(req, res) {
-    return req.headers['content-length'] || 0
-})
+morgan.token("content-length", function getContentLength(req, res) {
+    return req.headers["content-length"] || 0;
+});
 
-morgan.token('body', function getBody(req, res) {
-    return JSON.stringify(req.body)
-})
+morgan.token("body", function getBody(req, res) {
+    return JSON.stringify(req.body);
+});
 
-app.use(cors())
-app.use(bodyParser.json())
-app.use(morgan(function log(t, req, res) {
-    return [
-        t['method'](req, res),
-        t['url'](req, res),
-        t['status'](req, res),
-        t['content-length'](req, res), '-',
-        t['response-time'](req, res), 'ms',
-        t['body'](req, res)
-    ].join(' ')
-}))
+app.use(cors());
+app.use(bodyParser.json());
+app.use(
+    morgan(function log(t, req, res) {
+        return [
+            t["method"](req, res),
+            t["url"](req, res),
+            t["status"](req, res),
+            t["content-length"](req, res),
+            "-",
+            t["response-time"](req, res),
+            "ms",
+            t["body"](req, res),
+        ].join(" ");
+    })
+);
 
-app.use(express.static('build'))
+app.use(express.static("build"));
 
-var port = process.env.PORT || 3001
+var port = process.env.PORT || 3001;
 app.listen(port, () => {
-    console.log('Listening')
-})
+    console.log("Listening");
+});
 
-var persons = [
-    {
-        id: 1,
-        name: 'Arto Hellas',
-        number: '040-123456'
-    },
-    {
-        id: 2,
-        name: 'Ada Lovelace',
-        number: '39-44-5323523'
-    },
-    {
-        id: 3,
-        name: 'Dan Abramov',
-        number: '12-43-234345'
-    },
-    {
-        id: 4,
-        name: 'Mary Poppendick',
-        number: '39-23-5423122'
-    }
-]
-
-app.get('/api/persons/:id', async function getPerson(req, res, next) {
+app.get("/api/persons/:id", async function getPerson(req, res, next) {
     try {
-        var person = await db.getPersonById(req.params.id)
+        var person = await db.getPersonById(req.params.id);
         if (!person) {
-            throw new Error()
+            throw new Error();
         }
-    } catch(e) {
-        return next()
+    } catch (e) {
+        return next();
     }
-    res.json(person)
-})
+    res.json(person);
+});
 
-app.delete('/api/persons/:id', function deletePerson(req, res) {
+app.delete("/api/persons/:id", function deletePerson(req, res) {
     db.removePerson(req.params.id)
         .then(() => {
-            console.log("success")
+            console.log("success");
         })
         .catch(() => {
-            console.log("fail")
-        })
+            console.log("fail");
+        });
 
-    res.end()
-})
+    res.end();
+});
 
-app.get('/api/persons', function getPersons(req, res) {
-    db.getPersons().then(response => {
-        res.json(response)
-    })
-})
+app.get("/api/persons", function getPersons(req, res) {
+    db.getPersons().then((response) => {
+        res.json(response);
+    });
+});
 
-app.post('/api/persons', async function addPerson(req, res, next) {
-    var { name, number } = req.body
-
+app.post("/api/persons", async function addPerson(req, res, next) {
+    var { name, number } = req.body;
 
     try {
-        var resp = await db.addPerson(name, number)   
-    } catch(e) {
-        return next({ message: e, code: 400 })
+        var resp = await db.addPerson(name, number);
+    } catch (e) {
+        return next({ message: e, code: 400 });
     }
-    res.status(201).json(resp)
-})
+    res.status(201).json(resp);
+});
 
-app.put('/api/persons/:id', async function editPerson(req, res) {
-    var p = await db.updatePerson(req.params.id, req.body.number)
+app.put("/api/persons/:id", async function editPerson(req, res) {
+    var p = await db.updatePerson(req.params.id, req.body.number);
 
-    res.json(p)
-})
+    res.json(p);
+});
 
-app.get('/info', async function getInfo(req, res) {
-    var count = await db.getPersonCount()    
+app.get("/info", async function getInfo(req, res) {
+    var count = await db.getPersonCount();
 
-    res.send(`<p>Phonebook has info for ${count} people</p><p>${new Date()}</p>`)
-})
+    res.send(`<p>Phonebook has info for ${count} people</p><p>${new Date()}</p>`);
+});
 
-app.use(function (error, req, res, next) {
-    return res.status(error.code).json({ error: error.message })
+app.use(function (error, req, res) {
+    return res.status(error.code).json({ error: error.message });
+});
 
-    next()
-})
-
-app.use(function (req, res, next) {
-    res.status(404).json({ error: 'page not found' })
-})
+app.use(function (req, res) {
+    res.status(404).json({ error: "page not found" });
+});
--- osa3/puhelinluettelo-backend/mongo.js
+++ osa3/puhelinluettelo-backend/mongo.js
@@ -4,33 +4,33 @@ var [password, name, number] = process.argv.slice(2);
 var uri = `mongodb+srv://fullstack:${password}@cluster0.zx1mw.mongodb.net/fullstackmooc?retryWrites=true&w=majority`;
 
 mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
 });
 
 var schema = new mongoose.Schema({
-  name: String,
-  number: String,
+    name: String,
+    number: String,
 });
 
 var Person = mongoose.model("Person", schema);
 
 var person = new Person({
-  name,
-  number,
+    name,
+    number,
 });
 
 if (process.argv.length <= 3) {
-  console.log("phonebook:");
-  Person.find().then((result) => {
-    result.forEach((e) => console.log(e.name, e.number));
-    mongoose.disconnect();
-  });
+    console.log("phonebook:");
+    Person.find().then((result) => {
+        result.forEach((e) => console.log(e.name, e.number));
+        mongoose.disconnect();
+    });
 } else {
-  person.save((response) => {
-    console.log(`added ${name} number ${number} to phonebook`);
-    mongoose.disconnect();
-  });
+    person.save(() => {
+        console.log(`added ${name} number ${number} to phonebook`);
+        mongoose.disconnect();
+    });
 }
